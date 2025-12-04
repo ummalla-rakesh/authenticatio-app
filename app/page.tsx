6cb3cea51af0d48917dce50b1d3c4de43baf2fbc
ummalla Rakesh <58021360+ummalla-rakesh@users.noreply.github.com>
2025-12-04 11:24:28 +0530

    feat: add ThemeSwitcher component to AuthButton and remove from Home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
 import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
@@ -10,7 +9,6 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Home() {
-  
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -51,7 +49,6 @@ export default function Home() {
               Supabase
             </a>
           </p>
-          <ThemeSwitcher />
         </footer>
       </div>
     </main>
--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { createClient } from "@/lib/supabase/server";
 import { LogoutButton } from "./logout-button";
+import { ThemeSwitcher } from "./theme-switcher";
 
 export async function AuthButton() {
   const supabase = await createClient();
@@ -15,6 +16,7 @@ export async function AuthButton() {
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <LogoutButton />
+      <ThemeSwitcher />
     </div>
   ) : (
     <div className="flex gap-2">
@@ -24,6 +26,7 @@ export async function AuthButton() {
       <Button asChild size="sm" variant={"default"}>
         <Link href="/auth/sign-up">Sign up</Link>
       </Button>
+      <ThemeSwitcher />
     </div>
   );
 }
